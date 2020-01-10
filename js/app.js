8e936d04bccd80ec82464deb84b799d6c8bdab61
skg1998 <sahilkg1998@gmail.com>
2020-01-10 22:21:32 +0530

    Init

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+
+    $('bxslider').bxslider({
+        mode:'horizontal',
+        moveSlides:1,
+        slideMargin:40,
+        infinteloop:true,
+        minSlides:1,
+        maxSlides:1,
+        speed:1200,
+    });
+})
