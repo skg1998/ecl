8f89f54039de49e056b0d4c63d3965903e172aad
skg1998 <sahilkg1998@gmail.com>
2020-01-11 12:51:44 +0530

    init

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
 
-    $('bxslider').bxslider({
-        mode:'horizontal',
-        moveSlides:1,
-        slideMargin:40,
-        infinteloop:true,
-        minSlides:1,
-        maxSlides:1,
-        speed:1200,
-    });
-})
+//     $('bxslider').bxslider({
+//         mode:'horizontal',
+//         moveSlides:1,
+//         slideMargin:40,
+//         infinteloop:true,
+//         minSlides:1,
+//         maxSlides:1,
+//         speed:1200,
+//     });
+// })
+
+$('.bg-video video').width( window.innerWidth );
